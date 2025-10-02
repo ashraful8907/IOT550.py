2f79ef5dbaad7807b31aad206dc5f67a015fa34e
ashraful8907 <ashraful8907@gmail.com>
2025-10-02 11:51:57 +0100

    test

--- unit_testing.py
+++ unit_testing.py
@@ -1,13 +1,11 @@
 import unittest
-
-def add_numbers(a, b):
-    return a + b
+from add_numbers import add_numbers
 
 class TestFunction(unittest.TestCase):
 
     def test_addition(self):
         result = add_numbers(4, 6)
-        self.assertEqual(result, 10)
+        self.assertEqual(result, 12)
 
 if __name__ == "__main__":
     unittest.main()
